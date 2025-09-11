67a7de428b933a96faca6d030dfd6c291ceab586
Metro-Man <adriansvasiljevs07@gmail.com>
2025-09-11 16:55:42 +0300

    Im dumbass whom mixed header with main/ adding comments for directions

--- views/weather.php
+++ views/weather.php
@@ -12,49 +12,72 @@
         <div class="min-h-screen pl-6 pr-6 max-w-full bg-gray-100">
             <header class="flex justify-between items-center bg-gray-600  shadow-md rounded-b-lg px-4 py-6 sticky top-0 inset-x-0 mx-auto z-50">
 
-            </header>
-            <main class="mt-10">
-                <!-- this section is button, 3 lines, title -->
+   <!-- this section is button, 3 lines, title -->
                 <div class="flex items-center">
+                    <!-- this section is button, 3 lines, title -->
                     <button class="p-2 mr-3">
+                        <!-- this section is button, 3 lines, title -->
                         <svg class="text-gray-600 w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <!-- this section is button, 3 lines, title -->
                             <path d="M4 6h16M4 12h16M4 18h16" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
                         </svg>
                     </button>
+                    <!-- this section is button, 3 lines, title -->
                     <h1 class="text-2xl font-semibold text-blue-500 mr-3 cursor-pointer hidden md:block">VTDT Sky</h1>
+                    <!-- this section is button, 3 lines, title -->
                     <img src="./images/google-maps.gif" alt="Description of the image" class="w-6 h-6 ml-4 md:ml-6 lg:ml-8 xl:ml-14 hidden md:block">
+                    <!-- this section is button, 3 lines, title -->
                     <span class="text-white text-gray-800 ml-1 hidden md:block">Riga, Latvia</span>
                 </div>
 
                     <!-- this section is searchbar + toggle mode button, has svg, img, path d, form / button svg path d span -->
                     <div class="flex items-center space-x-4">
                         <div class="relative flex items-center">
+                            <!-- this section is button, 3 lines, title -->
                             <form>
+                                <!-- this section is button, 3 lines, title -->
                                 <input type="text" placeholder="Search Location" class="pl-10 pr-10 py-2 border-2 border-gray-300 rounded-lg text-sm w-full max-w-full min-w-[153px] " value="Cēsis">
-
+                                    <!-- this section is button, 3 lines, title -->
                                     <svg class="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-600" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <!-- this section is button, 3 lines, title -->
                                         <path d="M11 2a9 9 0 1 0 6.293 15.293l4.707 4.707a1 1 0 0 0 1.414-1.414l-4.707-4.707A9 9 0 0 0 11 2zM11 4a7 7 0 1 1 0 14 7 7 0 0 1 0-14z"></path>
                                     </svg>
+                                        <!-- this section is button, 3 lines, title -->
                                         <img src="./images/worldwide.gif" alt="Description of the image" class="absolute right-2.5 top-1/2 transform -translate-y-1/2 w-6 h-6  z-10">
                             </form>
+                            <!-- this section is button, 3 lines, title -->
                             <div class="absolute right-0.5 bg-gray-100 w-10 h-[90%] rounded-r-lg"></div>
                         </div>
+                        <!-- this section is button, 3 lines, title -->
                         <button class="relative flex items-center space-x-2 px-3 py-2 rounded-lg transition-colors duration-300 bg-gray-300 text-gray-900 hover:bg-gray-400 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700  hidden sm:flex">
+                            <!-- this section is button, 3 lines, title -->
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
+                                <!-- this section is button, 3 lines, title -->
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"></path>
                             </svg>
+                            <!-- this section is button, 3 lines, title -->
                             <span>Light</span>
                         </button>
                     </div>
 
                 <!-- this section is button, 3 lines, title -->
                 <div class="flex items-center space-x-4">
+                    <!-- this section is button, 3 lines, title -->
                     <button class="p-2 rounded-full hidden sm:block">
-                    
+                        <img src="./images/notification.gif" alt="Description of the image" class="size-7">
+                        <!-- this section is button, 3 lines, title -->
+                        </button>
+                            <img src="./images/profile-pic.jpg" alt="Description of the image" class="rounded-full w-9 h-9 hidden sm:block lg:hidden">
+                        <!-- this section is button, 3 lines, title -->
+                        <button class="p-2 rounded-full hidden lg:block">
+                            <img src="./images/settings.gif" alt="Description of the image" class="size-7">
                    </button>
-                   <img src="./images/profile-pic.jpg" alt="Description of the image" class="rounded-full w-9 h-9 hidden sm:block lg:hidden">
                    
                 </div>
+
+            </header>
+            <main class="mt-10">
+             
 
             </main>
         </div>
